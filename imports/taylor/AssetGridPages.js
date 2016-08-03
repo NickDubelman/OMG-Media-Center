3f1917838cdc12e6c9517e57a8b2b438465096be
Nick Dubelman <ndubelman@gmail.com>
2016-08-03 15:37:05 -0700

    styled pagination

--- imports/taylor/AssetGridPages.js
+++ imports/taylor/AssetGridPages.js
@@ -10,12 +10,16 @@ const AssetGridPages = ({currFolder, perPage, totalResults, goToPage, currPage})
     pages[i]=i+1
   }
   return(
-    <div>
-      {pages.map((page)=> 
-        <h1 key={'page'+page} style={pageNumber} onClick={goToPage.bind(this, currFolder, perPage, page, currPage)}>
-          {page}
-        </h1>
-      )}
+    <div style={assetGridPages}>
+      {pages.map((page)=> {
+        let styling = 'page-number'
+        if (page === currPage) styling+=' curr-page'
+        return(
+          <div key={'page'+page} className={styling} onClick={goToPage.bind(this, currFolder, perPage, page, currPage)}>
+            {page}
+          </div>
+        )
+      })}
     </div>
   )
 }
@@ -39,4 +43,12 @@ const mapDispatchToProps = dispatch => {
 
 export default connect(mapStateToProps, mapDispatchToProps)(AssetGridPages)
 
-let pageNumber = {display: 'inline-block', paddingRight: 10, cursor: 'pointer'}
+let assetGridPages = {
+  marginTop: 20,
+  marginBottom: 30,
+  marginLeft: 15
+}
+
+let pageNumberActive = {
+  backgroundColor: '#4bab49',
+}
