019a4101059645829e3881e08f78dc07ececae28
Nick Dubelman <Nick@Nicks-MacBook-Pro.local>
2016-08-22 11:43:56 -0700

    testing font size change2

--- imports/components/MobileNavbar.js
+++ imports/components/MobileNavbar.js
@@ -11,7 +11,7 @@ const MobileNavbar = ({menuItems}) => (
   		</Navbar.Brand>
   		<Navbar.Toggle />
   	</Navbar.Header>
-  	<Navbar.Collapse style={{fontSize: 10}}>
+  	<Navbar.Collapse style={{fontSize: 16}}>
   		<Nav pullRight>
         {menuItems.map((item,i)=>(
           <NavDropdown key={'NavDropdown-'+item.label+'-'+i} title={item.label} id={item.label+'-dropdown'}>
